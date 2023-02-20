6350cd4b294dfe9be8f23e7103d5f22ab29b9c16
TheApostole <yaloza99@mail.ru>
2023-02-20 05:21:05 +0300

    "Stream API и Optional", Дз выполнено

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,42 @@
+import java.awt.im.InputSubset;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import java.util.Comparator;
+import java.util. function.BiConsumer;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //Задание 1
+        List<Integer> numbers = Arrays.asList(23, 432, 3, 4, 32, 4, 3232, 44, 48, 2643, 243, 25);
+        Stream<Integer> numbersStream = numbers.stream();
+        Comparator<Integer> numbersComparator = Comparator.comparing(Integer::intValue);
+        BiConsumer<Integer, Integer> numbersBiConsumer = (min, max) -> System.out.println("Минимальное число: " + min + ", Максимальное число: " + max);
+        findMinMax(numbersStream, numbersComparator, numbersBiConsumer);
+
+        //Задание 2
+        determinesTheQuantityOfEvenNumbersAndOutputsThem(numbers);
+    }
+
+    /** Задание 1 */
+    public static <T> void findMinMax(Stream <? extends T> stream,
+                                      Comparator <? super T> order,
+                                      BiConsumer <? super T, ? super T> minMaxConsumer) {
+        List<T> list = stream.collect(Collectors.toList());
+        if (list.isEmpty()) {
+            minMaxConsumer.accept(null, null);
+        }else {
+            list.sort(order);
+            minMaxConsumer.accept(list.get(0), list.get(list.size() - 1));
+        }
+    }
+
+    /** Задание 2 */
+    public static void determinesTheQuantityOfEvenNumbersAndOutputsThem(List<Integer> numbersList) {
+        Stream<Integer> numbersStream  = numbersList.stream().filter(n -> n % 2 == 0);
+        List<Integer> collect = numbersStream.collect(Collectors.toList());
+        System.out.println("Количество четных чисел: " + collect.size());
+        System.out.println(collect);
     }
 }
